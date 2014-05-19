30f623e289ffecbc32a7ed6d7578fed0b2bfdc4c
Andrew VanderVeen <avanderveen@ea.com>
2014-05-19 19:34:05 -0400

    use blocks for popen so that ruby waits for child process to exit

--- files/default/JvmHeapUsage.rb
+++ files/default/JvmHeapUsage.rb
@@ -63,12 +63,13 @@ module God
 				end
 
 				# Get the child pids.
-				pipe = IO.popen("ps aux | grep jetty | grep start.jar | grep -v grep | awk '{print $2}'")
-
-				jetty_pid = pipe.readlines.map do |line|
-					parts = line.split(/\s+/)
-					parts[0]
-				end.compact
+				jetty_ps_cmd = "ps aux | grep jetty | grep start.jar | grep -v grep | awk '{print $2}'"
+				jetty_pid = IO.popen(jetty_ps_cmd) do |jetty|
+					jetty.readlines.map do |line|
+						parts = line.split(/\s+/)
+						parts[0]
+					end.compact
+				end
 
 				# Show the child processes.
 				jetty_pid.size == 0 ? nil : jetty_pid[0]
@@ -83,12 +84,14 @@ module God
 				# C    S1C    S0U    S1U      EC       EU        OC         OU       PC     PU    YGC     YGCT    FGC    FGCT     GCT
 				# 192.0  192.0   0.0    0.0    2048.0   1244.0    4864.0      0.0     21248.0 2908.3      0    0.000   0      0.000    0.000
 				# we care about S0U/S1U (survivor space) & EU (eden space) & OU (old space)
-				results = IO.popen("jstat -gc #{pid}").readlines
-				if results.size > 0
-					headers = results[0].strip.split(/\s+/)
-					heapUsage = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
-						["S0U", "S1U", "EU", "OU"].include?(headers[i]) ? value.to_f : nil
-					}.compact.inject{|sum, x| sum + x}
+				IO.popen("jstat -gc #{pid}") do |jstat|
+					results = jstat.readlines
+					if results.size > 0
+						headers = results[0].strip.split(/\s+/)
+						heapUsage = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
+							["S0U", "S1U", "EU", "OU"].include?(headers[i]) ? value.to_f : nil
+						}.compact.inject{|sum, x| sum + x}
+					end
 				end
 
 				return heapUsage
@@ -100,12 +103,14 @@ module God
 				# NGCMN      NGCMX       NGC      S0CMX     S0C     S1CMX     S1C       ECMX        EC      YGC   FGC
 				# 2432.0    43008.0     2432.0   4288.0    192.0   4288.0    192.0    34432.0     2048.0     0     0
 				# we care about S0CMX/S1CMX (max survivor space capacity) & ECMX (max eden space capcity)
-				results = IO.popen("jstat -gcnewcapacity #{pid}").readlines
-				if results.size > 0
-					headers = results[0].strip.split(/\s+/)
-					newGenCap = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
-						["S0CMX", "ECMX"].include?(headers[i]) ? value.to_f : nil
-					}.compact.inject{|sum, x| sum + x}
+				IO.popen("jstat -gcnewcapacity #{pid}") do |jstat|
+					results = jstat.readlines
+					if results.size > 0
+						headers = results[0].strip.split(/\s+/)
+						newGenCap = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
+							["S0CMX", "ECMX"].include?(headers[i]) ? value.to_f : nil
+						}.compact.inject{|sum, x| sum + x}
+					end
 				end
 
 				return newGenCap
@@ -117,12 +122,14 @@ module God
 				# OGCMN       OGCMX        OGC         OC       YGC   FGC    FGCT     GCT
 				# 4864.0     86016.0      4864.0      4864.0     0     0    0.000    0.000
 				# we care about OGCMX (max old generation capacity)
-				results = IO.popen("jstat -gcoldcapacity #{pid}").readlines
-				if results.size > 0
-					headers = results[0].strip.split(/\s+/)
-					oldGenCap = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
-						["OGCMX"].include?(headers[i]) ? value.to_f : nil
-					}.compact.inject{|sum, x| sum + x}
+				IO.popen("jstat -gcoldcapacity #{pid}") do |jstat|
+					results = jstat.readlines
+					if results.size > 0
+						headers = results[0].strip.split(/\s+/)
+						oldGenCap = results[1].strip.split(/\s+/).each_with_index.map {|value, i|
+							["OGCMX"].include?(headers[i]) ? value.to_f : nil
+						}.compact.inject{|sum, x| sum + x}
+					end
 				end
 
 				return oldGenCap
