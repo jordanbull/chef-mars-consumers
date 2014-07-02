749fb6ba72c6bb9d3cfd940c7d1f3b8b6ae17ecd
Andrew VanderVeen <avanderveen@ea.com>
2014-07-02 13:56:40 -0400

    move lines below required declaration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
-# mars-specific properties
-default[:mars][:app_logs] = "/var/log/#{node[:war][:name]}/#{node[:war][:name]}.log"
-default[:mars][:properties] = "int"
-
 # general project attributes
 default[:war][:name] = "mars"
 default[:war][:version] = "1.0-SNAPSHOT"
 default[:war][:environment] = "dev"
+
+# mars-specific properties
+default[:mars][:app_logs] = "/var/log/#{node[:war][:name]}/#{node[:war][:name]}.log"
+default[:mars][:properties] = "int"
 
 # general tnt deployment attributes
 default[:war][:source][:warfile] = "#{node[:war][:name]}-#{node[:war][:version]}.war"
