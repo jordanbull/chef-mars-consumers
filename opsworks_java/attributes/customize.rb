0f8acc85a0af55ca76394a3b087f181ed463b8a1
awesometown <john.price@gmail.com>
2014-07-25 15:16:04 -0400

    Update queuenames to use mars-<env>-<partner> format.

--- opsworks_java/attributes/customize.rb
+++ opsworks_java/attributes/customize.rb
@@ -1,16 +1,18 @@
 default[:mars][:env] = "dev"
 case node[:opsworks][:instance][:layers].first
 when 'mars-burstly-consumers'
-	default[:mars][:queueName] = "mars-burstly-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-burstly"
 when 'mars-nanigans-consumers'
-	default[:mars][:queueName] = "mars-nanigans-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-nanigans"
 when 'mars-fiksu-ios-consumers'
-	default[:mars][:queueName] = "mars-fiksu_ios-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-fiksu_ios"
 when 'mars-fiksu-android-consumers'
-	default[:mars][:queueName] = "mars-fiksu_android-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-fiksu_android"
+when 'mars-fiksu-consumers'
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-fiksu"
 when 'mars-mat-consumers'
-	default[:mars][:queueName] = "mars-mat-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-mat"
 when 'mars-kochava-consumers'
-	default[:mars][:queueName] = "mars-kochava-#{node[:mars][:env]}"
+	default[:mars][:queueName] = "mars-#{node[:mars][:env]}-kochava"
 end
 default['opsworks_java']['tomcat']['java_opts'] = "#{node[:opsworks_java][:jvm_options]} -Dspring.profiles.active=consumer -Dmars.env=#{node[:mars][:env]} -Dmars.consumer.inputqueue=#{node[:mars][:queueName]}"
