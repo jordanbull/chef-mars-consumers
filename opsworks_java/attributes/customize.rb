de016f06324ced8f31d6f5adcbff387a267ee37c
Yulin Chen <yuchen@ea.com>
2014-07-25 15:28:08 -0400

    allow user to provide custom layer name to queue mapping throught attribute [:mars][:consumers][:layer_to_queuemap]

--- opsworks_java/attributes/customize.rb
+++ opsworks_java/attributes/customize.rb
@@ -1,16 +1,34 @@
 default[:mars][:env] = "dev"
-case node[:opsworks][:instance][:layers].first
-when 'mars-burstly-consumers'
+
+default[:mars][:consumers][:layer_to_queuemap] = {
+	"mars-burstly-consumers"       => "burstly",
+	"mars-nanigans-consumers"      => "nanigans",
+	"mars-fiksu-ios-consumers"     => "fiksu_ios",
+	"mars-fiksu-android-consumers" => "fiksu_android",
+	"mars-mat-consumers"           => "mat",
+	"mars-kochava-consumers"       => "kochava"
+}
+
+layers = node[:opsworks][:instance][:layers]
+layer_to_queuemap = node[:mars][:consumers][:layer_to_queuemap]
+queue = if layers.size == 1 && layer_to_queuemap.has_key?(layers.first)
+			layer_to_queuemap[layers.first]
+		else
+			raise "Coule not find mapping in 'node[:mars][:consumers][:layer_to_queuemap]' for key '#{layer_to_queuemap[layers.first]}'"
+		end
+
+case queue
+when 'burstly'
 	default[:mars][:queueName] = "mars-burstly-#{node[:mars][:env]}"
-when 'mars-nanigans-consumers'
+when 'nanigans'
 	default[:mars][:queueName] = "mars-nanigans-#{node[:mars][:env]}"
-when 'mars-fiksu-ios-consumers'
+when 'fiksu-ios'
 	default[:mars][:queueName] = "mars-fiksu_ios-#{node[:mars][:env]}"
-when 'mars-fiksu-android-consumers'
+when 'fiksu-android'
 	default[:mars][:queueName] = "mars-fiksu_android-#{node[:mars][:env]}"
-when 'mars-mat-consumers'
+when 'mat'
 	default[:mars][:queueName] = "mars-mat-#{node[:mars][:env]}"
-when 'mars-kochava-consumers'
+when 'kochava'
 	default[:mars][:queueName] = "mars-kochava-#{node[:mars][:env]}"
 end
 default['opsworks_java']['tomcat']['java_opts'] = "#{node[:opsworks_java][:jvm_options]} -Dspring.profiles.active=consumer -Dmars.env=#{node[:mars][:env]} -Dmars.consumer.inputqueue=#{node[:mars][:queueName]}"
