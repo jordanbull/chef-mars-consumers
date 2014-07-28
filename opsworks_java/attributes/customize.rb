6c98d2e41d2e74ad77f7f8f215083587b7732892
Yulin Chen <yuchen@ea.com>
2014-07-28 10:45:22 -0400

    remove magic string mapping, better error handling messages

--- opsworks_java/attributes/customize.rb
+++ opsworks_java/attributes/customize.rb
@@ -1,34 +1,17 @@
 default[:mars][:env] = "dev"
 
-default[:mars][:consumers][:layer_to_queuemap] = {
-	"mars-burstly-consumers"       => "burstly",
-	"mars-nanigans-consumers"      => "nanigans",
-	"mars-fiksu-ios-consumers"     => "fiksu_ios",
-	"mars-fiksu-android-consumers" => "fiksu_android",
-	"mars-mat-consumers"           => "mat",
-	"mars-kochava-consumers"       => "kochava"
-}
-
 layers = node[:opsworks][:instance][:layers]
-layer_to_queuemap = node[:mars][:consumers][:layer_to_queuemap]
-queue = if layers.size == 1 && layer_to_queuemap.has_key?(layers.first)
-			layer_to_queuemap[layers.first]
+layer_to_queue_map = if node[:mars].nil? || node[:mars][:consumers].nil? || node[:mars][:consumers][:layer_to_queue_map].nil?
+						raise "node[:mars][:consumers][:layer_to_queue_map] is not set"
+					else
+						node[:mars][:consumers][:layer_to_queue_map]
+					end
+queue = if layers.size != 1
+			raise "layers.size is not 1"
+		elsif !layer_to_queue_map.has_key?(layers.first)
+			raise "Coule not find mapping in 'node[:mars][:consumers][:layer_to_queue_map]' for key '#{layers.first}'"
 		else
-			raise "Coule not find mapping in 'node[:mars][:consumers][:layer_to_queuemap]' for key '#{layer_to_queuemap[layers.first]}'"
+			layer_to_queue_map[layers.first]
 		end
 
-case queue
-when 'burstly'
-	default[:mars][:queueName] = "mars-burstly-#{node[:mars][:env]}"
-when 'nanigans'
-	default[:mars][:queueName] = "mars-nanigans-#{node[:mars][:env]}"
-when 'fiksu-ios'
-	default[:mars][:queueName] = "mars-fiksu_ios-#{node[:mars][:env]}"
-when 'fiksu-android'
-	default[:mars][:queueName] = "mars-fiksu_android-#{node[:mars][:env]}"
-when 'mat'
-	default[:mars][:queueName] = "mars-mat-#{node[:mars][:env]}"
-when 'kochava'
-	default[:mars][:queueName] = "mars-kochava-#{node[:mars][:env]}"
-end
-default['opsworks_java']['tomcat']['java_opts'] = "#{node[:opsworks_java][:jvm_options]} -Dspring.profiles.active=consumer -Dmars.env=#{node[:mars][:env]} -Dmars.consumer.inputqueue=#{node[:mars][:queueName]}"
+default['opsworks_java']['tomcat']['java_opts'] = "#{node[:opsworks_java][:jvm_options]} -Dspring.profiles.active=consumer -Dmars.env=#{node[:mars][:env]} -Dmars.consumer.inputqueue=#{queue}"
